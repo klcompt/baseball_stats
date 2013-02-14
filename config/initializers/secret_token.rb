2bead9f730bd72287453f569f8cf979a5dbc1393
Kevin Compton <klcompt@gmail.com>
2013-02-13 21:46:17 -0600

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaseballStats::Application.config.secret_token = 'ee666bbd214335c931d1b8aaea827cc8358d18056499073ec715ed7620067d75633d67bdb45a4cd48d892c260b25406c6a945acb568fce86ee641d7672262fe7'
