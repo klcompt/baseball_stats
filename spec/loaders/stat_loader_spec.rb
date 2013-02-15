e54b4d52b30010e7fe7ec62724cdeb1abda9bc4a
Kevin Compton <klcompt@gmail.com>
2013-02-14 18:53:31 -0600

    Fixes typo

--- spec/loaders/stat_loader_spec.rb
+++ spec/loaders/stat_loader_spec.rb
@@ -7,7 +7,7 @@ describe 'StatLoader' do
     loader = StatLoader.new
     stat_data = {player_id: 'test123', year: '2011', team_id: 'NYA', game: '74',
       at_bats: '6', runs: '5', hits: '3', doubles: nil, triples: '1', home_runs: '0',
-      runs_batted_in: '9', stolen_bases: '8', caugh_stealing: '1'}
+      runs_batted_in: '9', stolen_bases: '8', caught_stealing: '1'}
 
     Stat.should_receive(:create).with(stat_data)
 
