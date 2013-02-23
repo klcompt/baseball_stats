f47c764bd889fc154e592c21d5163499394f8a3d
Kevin Compton <klcompt@gmail.com>
2013-02-22 21:44:59 -0600

    Using newer migration syntax

--- db/migrate/20130223022913_add_indexes.rb
+++ db/migrate/20130223022913_add_indexes.rb
@@ -1,17 +1,9 @@
 class AddIndexes < ActiveRecord::Migration
-  def up
+  def change
     add_index :players, :id
     add_index :players, :player_key
     add_index :stats, :player_id
     add_index :stats, :year
     add_index :stats, :team_id
-  end
-
-  def down
-    drop_index :players, :id
-    drop_index :players, :player_key
-    drop_index :stats, :player_id
-    drop_index :stats, :year
-    drop_index :stats, :team_id
   end
 end
